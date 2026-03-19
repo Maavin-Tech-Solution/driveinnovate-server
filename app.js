15dba9f0567cfe586a99a114fc5efd13fa226ea0
amarpandey <amar.itdeveloper@gmail.com>
2026-03-19 23:41:29 +0530

    db connection updated

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
+  'http://127.0.0.1:3000',
   'https://driveinnovate.in',
   'https://stage.driveinnovate.in',
 ];
